e00629bbd983d2714fd79273f3fb88d3b941981c
danishzulfiqar <danishzulfiqar100@gmail.com>
2025-03-03 08:29:14 +0500

    added changes to frontend

--- app.py
+++ app.py
@@ -57,9 +57,38 @@ async def predict(request: Request, text: str = Form(...)):
         "home.html", 
         {
             "request": request,
-            "pred": f"The above text is in {my_pred[0]}"
+            "pred": f"Language: {my_pred[0]}"
         }
     )
+
+# predict text
+@app.post("/predict_text")
+async def predict_text(text: str = Form(...)):
+    # loading the dataset
+    data = pd.read_csv("language_detection.csv")
+    y = data["Language"]
+
+    # label encoding
+    y = le.fit_transform(y)
+
+    # loading the model and cv
+    model = pickle.load(open("model.pkl", "rb"))
+    cv = pickle.load(open("transform.pkl", "rb"))
+
+    # preprocessing the text
+    text = re.sub(r'[!@#$(),\n"%^*?\:;~`0-9]', '', text)
+    text = re.sub(r'[[]]', '', text)
+    text = text.lower()
+    dat = [text]
+    
+    # creating the vector
+    vect = cv.transform(dat).toarray()
+    
+    # prediction
+    my_pred = model.predict(vect)
+    my_pred = le.inverse_transform(my_pred)
+
+    return {"prediction": f"{my_pred[0]}"}
 
 if __name__ == "__main__":
     import uvicorn
